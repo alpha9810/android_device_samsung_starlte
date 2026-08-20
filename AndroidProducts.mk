71bafee4554cf82579528c7315f2b758c5cb1389
saifanidk <saifan7787790@gmail.com>
2026-08-20 20:48:56 +0600

    Update product makefiles for alpha_starlte

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,9 +2,9 @@
 # Copyright (C) 2022 The LineageOS Project
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_starlte.mk
+    $(LOCAL_DIR)/alpha_starlte.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_starlte-eng \
-    lineage_starlte-user \
-    lineage_starlte-userdebug
+    alpha_starlte-eng \
+    alpha_starlte-user \
+    alpha_starlte-userdebug
