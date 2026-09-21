820c082e89d4d720033d5ba439952e69d31c4d44
Localhorst04 <aaron.laib@protonmail.com>
2026-09-21 16:05:02 +0200

    voltageos: bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,9 +2,9 @@
 # Copyright (C) 2022 The LineageOS Project
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_starlte.mk
+    $(LOCAL_DIR)/voltage_starlte.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_starlte-eng \
-    lineage_starlte-user \
-    lineage_starlte-userdebug
+    voltage_starlte-eng \
+    voltage_starlte-user \
+    voltage_starlte-userdebug
--- voltage_starlte.mk
+++ voltage_starlte.mk
@@ -12,10 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 $(call inherit-product, device/samsung/starlte/device.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
 # Device identifier, this must come after all inclusions
-PRODUCT_NAME := lineage_starlte
+PRODUCT_NAME := voltage_starlte
 PRODUCT_DEVICE := starlte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G960F
